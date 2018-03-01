f5ef8abdd1e7867bfac32d88625ec121beec6615
Troy Williams <troyDonwilliams@gmail.com>
2018-02-28 22:13:22 -0600

    early structure set up

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./App.css";
+
+// Components
+import Navbar from "./components/Navbar/Navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Navbar />
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+class Navbar extends PureComponent {
+  render() {
+    return <div>Navbar Component</div>;
+  }
+}
+
+export default Navbar;
